5d5c7444bd5121dbb7383de525f81ee17102417a
Sathmi <sathmi.20211222@iit.ac.lk>
2023-03-24 12:16:59 +0530

    login and signup connected to frontend
    Removed wtforms.
    Create doctor and patient signup and login connected with frontend html template.

--- Flask_React/flask-server/api/app.py
+++ Flask_React/flask-server/api/app.py
@@ -1,17 +1,29 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect , request, flash
 from flask_sqlalchemy import SQLAlchemy 
 from flask_login import UserMixin, login_user, LoginManager, login_required,logout_user,current_user
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, ValidationError
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, PasswordField, SubmitField
+# from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
 from models import db
-from models import Patient
+from models import Patient,Doctor
+
+# To create the db:
+# python
+# from models import db, Doctor, Patient 
+# from app import app
+# db.create_all()
+# quit()/exit()
+
+# To get PATH of sqlite3:
+# $env:PATH += ';C:\SQLite3'
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/User/GitHub/EmoCare/Flask_React/flask-server/api/emocare.db'
 app.config['SECRET_KEY'] = 'thisisasecretkey'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 # db = SQLAlchemy(app)
 db.init_app(app)
 bcrypt = Bcrypt(app)
@@ -20,89 +32,115 @@ db.create_all()
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = "Patient"
+login_manager.login_view = "patientLogIn"
+# login_manager.login_view = "doctorLogIn"
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
-
-class RegisterForm(FlaskForm):
-    username = StringField(validators={InputRequired(), Length(min=4, max=20)}, render_kw={"placceholder": "Username"})
-    password = PasswordField(validators={InputRequired(), Length(min=4, max=20)}, render_kw={"placceholder": "Password"})
-    submit = SubmitField("Register")
-
-    def validate_username(self, username):
-        existing_user_username = Patient.query.filter_by(username=username.data).first()
-        if existing_user_username:
-            raise ValidationError(
-                "The username already exists. Enter a different one"
-            )
-
-class LoginForm(FlaskForm):
-    username = StringField(validators={InputRequired(), Length(min=4, max=20)}, render_kw={"placceholder": "Username"})
-    password = PasswordField(validators={InputRequired(), Length(min=4, max=20)}, render_kw={"placceholder": "Password"})
-    submit = SubmitField("Login")
+    return Patient.query.get(int(user_id))
 
 @app.route('/')
 def home():
-    return 'Hello from flask'
+    return render_template('home.html')
+ 
 
 @app.route('/dashboard')
-@login_required
 def dashboard():
-    return "welcome to dashboard"
+    return render_template('dashboard.html')
 
-@app.route('/patient', methods=['GET','POST'])
-def patientLogin():
-    form = LoginForm()
+@app.route('/patient/signup')
+def patientSignIn():
+   return render_template('patientSignup.html')
 
-    if form.validate_on_submit():
-        patient = Patient.query.filter_by(username=form.username.data).first()
-        if patient:
-            if bcrypt.check_password_hash(patient.password,form.password.data):
-               login_user(patient)
-               return redirect(url_for('dashboard'))
+@app.route('/patient/login')
+def patientLogIn():
+   return render_template('patientLogin.html')
 
-    return render_template('patient.html', form=form)
-
+@app.route('/patient/signup', methods=['POST'])
 def patientSignUp():
-    form = RegisterForm()
+   
+    name = request.form.get('name')
+    email = request.form.get('email')
+    username = request.form.get('username')
+    password = request.form.get('password' , 'none')
 
-# encrypting and creating a hash password
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_patient = Patient(username=form.username.data,password=hashed_password)
-        db.session.add(new_patient)
-        db.session.commit()
-        return redirect(url_for('patient'))
+    # check for exiting username and email !!
+    patient = Patient.query.filter_by(username=username).first()
+    if patient:
+        flash('Username already exists')
+        return redirect(url_for('patientSignIn'))
 
-    return render_template('Patient.html', form = form)
+    # encrypting and creating a hash password
+    hashed_password = bcrypt.generate_password_hash(password)
+    new_patient = Patient(name=name,email=email, username=username, password=hashed_password)
+    db.session.add(new_patient)
+    db.session.commit()
+    return redirect(url_for('patientLogIn')) 
 
-@app.route('/doctor', methods=['GET','POST'])
-def doctorLogin():
-    form = LoginForm()
 
-    if form.validate_on_submit():
-        doctor = Doctor.query.filter_by(username=form.username.data).first()
-        if doctor:
-            if bcrypt.check_password_hash(doctor.password,form.password.data):
-               login_user(doctor)
-               return redirect(url_for('dashboard'))
+@app.route('/patient/login', methods=['POST'])
+def patientLogin():
+    username = request.form.get('username')
+    password = request.form.get('password')
 
-    return render_template('doctor.html', form=form)
+    patient = Patient.query.filter_by(username=username).first()
+    if patient:
+        if bcrypt.check_password_hash(patient.password,password):
+            login_user(patient)
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Please check your login details and try again')
+            return redirect(url_for('patientLogIn'))
+    else:
+        flash('User does not exist')
+        return redirect(url_for('patientSignIn'))        
 
+@app.route('/doctor/signup')
+def doctorSignIn():
+   return render_template('doctorSignup.html')
+
+@app.route('/doctor/login')
+def doctorLogIn():
+   return render_template('doctorLogin.html')
+
+@app.route('/doctor/signup', methods=['POST'])
 def doctorSignUp():
-    form = RegisterForm()
+   
+    doctorName = request.form.get('doctorName')
+    doctorEmail = request.form.get('doctorEmail')
+    doctorUsername = request.form.get('doctorUsername')
+    doctorPassword = request.form.get('doctorPassword' , 'none')
 
-# encrypting and creating a hash password
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_doctor = Doctor(username=form.username.data,password=hashed_password)
-        db.session.add(doctor)
-        db.session.commit()
-        return redirect(url_for('/'))
+    # check for exiting username and email !!
+    doctor = Doctor.query.filter_by(doctorUsername=doctorUsername).first()
+    if doctor:
+        flash('Username already exists in the doctor database')
+        return redirect(url_for('doctorSignIn'))
 
-    return render_template('doctor.html',form =form)
+    # encrypting and creating a hash password
+    hashed_password = bcrypt.generate_password_hash(doctorPassword)
+    new_doctor = Doctor(doctorName=doctorName,doctorEmail=doctorEmail, doctorUsername=doctorUsername, doctorPassword=hashed_password)
+    db.session.add(new_doctor)
+    db.session.commit()
+    return redirect(url_for('doctorLogIn')) 
+
+
+@app.route('/doctor/login', methods=['POST'])
+def doctorLogin():
+    doctorUsername = request.form.get('doctorUsername')
+    doctorPassword = request.form.get('doctorPassword')
+
+    doctor = Doctor.query.filter_by(doctorUsername=doctorUsername).first()
+    if doctor:
+        if bcrypt.check_password_hash(doctor.doctorPassword,doctorPassword):
+            # login_user(doctor)
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Please check your login details and try again')
+            return redirect(url_for('doctorLogIn'))
+    else:
+        flash('User does not exist')
+        return redirect(url_for('doctorSignIn'))      
 
 @app.route('/logout', methods=['GET','POST'])
 @login_required
--- Flask_React/flask-server/api/models.py
+++ Flask_React/flask-server/api/models.py
@@ -3,12 +3,17 @@ from flask_login import UserMixin
 
 db = SQLAlchemy()
 
+
 class Patient(db.Model , UserMixin):
     id = db.Column(db.Integer , primary_key=True)
-    username = db.Column(db.String(20), unique=True , nullable=False)
-    password = db.Column(db.String(80), nullable=False)
+    name = db.Column(db.String(1000))
+    email = db.Column(db.String(100))
+    username = db.Column(db.String(20), unique=True)
+    password = db.Column(db.String(100))
 
-class Doctor(db.Model, UserMixin)
-    id = db.Column(db.Integer , primary_key=True)
-    username = db.Column(db.String(20), unique=True , nullable=False)
-    password = db.Column(db.String(80), nullable=False)
+class Doctor(db.Model, UserMixin):
+    doctorId = db.Column(db.Integer , primary_key=True)
+    doctorUsername = db.Column(db.String(20), unique=True)
+    doctorName =  db.Column(db.String(1000))
+    doctorEmail = db.Column(db.String(100))
+    doctorPassword = db.Column(db.String(80))
