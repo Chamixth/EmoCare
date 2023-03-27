9c6ad47616ed09889cd234f44c798a0bc6ea81ab
Sathmi <sathmi.20211222@iit.ac.lk>
2023-03-27 21:11:39 +0530

    adding changes of api for signin signup
    database models changes and routes changes (login to signin)

--- Flask_React/flask-server/api/app.py
+++ Flask_React/flask-server/api/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, url_for, redirect , request, flash
 from flask_sqlalchemy import SQLAlchemy 
-from flask_login import UserMixin, login_user, LoginManager, login_required,logout_user,current_user
+from flask_login import login_manager, UserMixin, login_user, LoginManager, login_required,logout_user,current_user
+from flask_security import Security, SQLAlchemySessionUserDatastore
+  
 # from flask_wtf import FlaskForm
 # from wtforms import StringField, PasswordField, SubmitField
 # from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
 from models import db
-from models import Patient,Doctor
+from models import Patient,Doctor,User,Role,user_roles
 
 # To create the db:
 # python
@@ -17,6 +19,7 @@ from models import Patient,Doctor
 
 # To get PATH of sqlite3:
 # $env:PATH += ';C:\SQLite3'
+# & sqlite3.exe
 
 app = Flask(__name__)
 
@@ -32,115 +35,84 @@ db.create_all()
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = "patientLogIn"
-# login_manager.login_view = "doctorLogIn"
+login_manager.login_view = "UserLogIn"
 
 @login_manager.user_loader
 def load_user(user_id):
-    return Patient.query.get(int(user_id))
+    return User.query.get(int(user_id))
+
+# load users, roles for a session
+user_datastore = SQLAlchemySessionUserDatastore(db.session, User, Role)
+security = Security(app, user_datastore)
 
 @app.route('/')
 def home():
     return render_template('home.html')
  
 
-@app.route('/dashboard')
-def dashboard():
-    return render_template('dashboard.html')
+@app.route('/patient/dashboard')
+def patientDashboard():
+    return "Welcome to patient dashboard"
 
-@app.route('/patient/signup')
-def patientSignIn():
-   return render_template('patientSignup.html')
+@app.route('/doctor/dashboard')
+def doctorDashboard():
+    return "Welcome to doctor dashboard"
 
-@app.route('/patient/login')
-def patientLogIn():
-   return render_template('patientLogin.html')
 
-@app.route('/patient/signup', methods=['POST'])
-def patientSignUp():
+@app.route('/signup')
+def UserSignIn():
+   return render_template('signup.html')
+
+@app.route('/signin')
+def UserLogIn():
+   return render_template('signin.html')
+
+@app.route('/signup', methods=['POST'])
+def SignUp():
    
     name = request.form.get('name')
     email = request.form.get('email')
     username = request.form.get('username')
-    password = request.form.get('password' , 'none')
+    password = request.form.get('password')
 
     # check for exiting username and email !!
-    patient = Patient.query.filter_by(username=username).first()
-    if patient:
+    user = User.query.filter_by(username=username).first()
+    if user:
         flash('Username already exists')
-        return redirect(url_for('patientSignIn'))
+        return redirect(url_for('UserSignIn'))
 
     # encrypting and creating a hash password
     hashed_password = bcrypt.generate_password_hash(password)
-    new_patient = Patient(name=name,email=email, username=username, password=hashed_password)
-    db.session.add(new_patient)
+    role = Role.query.filter_by(id=request.form['options']).first()
+    role_type = request.form['options']
+    new_user = User(name=name,email=email, username=username, password=hashed_password, active=1 ,roles=[role], role_id= role_type)
+    db.session.add(new_user)
     db.session.commit()
-    return redirect(url_for('patientLogIn')) 
+    return redirect(url_for('UserLogIn')) 
 
 
-@app.route('/patient/login', methods=['POST'])
-def patientLogin():
+@app.route('/signin', methods=['POST'])
+def SignIn():
     username = request.form.get('username')
     password = request.form.get('password')
 
-    patient = Patient.query.filter_by(username=username).first()
-    if patient:
-        if bcrypt.check_password_hash(patient.password,password):
-            login_user(patient)
-            return redirect(url_for('dashboard'))
+    user = User.query.filter_by(username=username).first()
+    if user:
+        if bcrypt.check_password_hash(user.password,password):
+            role_type = user.role_id
+            if (role_type == 1):
+                login_user(user)
+                return redirect(url_for('doctorDashboard'))
+            if (role_type == 2):
+                login_user(user)
+                return redirect(url_for('patientDashboard'))
         else:
             flash('Please check your login details and try again')
-            return redirect(url_for('patientLogIn'))
+            return redirect(url_for('UserLogIn'))
     else:
         flash('User does not exist')
-        return redirect(url_for('patientSignIn'))        
+        return redirect(url_for('UserSignIn'))        
 
-@app.route('/doctor/signup')
-def doctorSignIn():
-   return render_template('doctorSignup.html')
-
-@app.route('/doctor/login')
-def doctorLogIn():
-   return render_template('doctorLogin.html')
-
-@app.route('/doctor/signup', methods=['POST'])
-def doctorSignUp():
-   
-    doctorName = request.form.get('doctorName')
-    doctorEmail = request.form.get('doctorEmail')
-    doctorUsername = request.form.get('doctorUsername')
-    doctorPassword = request.form.get('doctorPassword' , 'none')
-
-    # check for exiting username and email !!
-    doctor = Doctor.query.filter_by(doctorUsername=doctorUsername).first()
-    if doctor:
-        flash('Username already exists in the doctor database')
-        return redirect(url_for('doctorSignIn'))
-
-    # encrypting and creating a hash password
-    hashed_password = bcrypt.generate_password_hash(doctorPassword)
-    new_doctor = Doctor(doctorName=doctorName,doctorEmail=doctorEmail, doctorUsername=doctorUsername, doctorPassword=hashed_password)
-    db.session.add(new_doctor)
-    db.session.commit()
-    return redirect(url_for('doctorLogIn')) 
-
-
-@app.route('/doctor/login', methods=['POST'])
-def doctorLogin():
-    doctorUsername = request.form.get('doctorUsername')
-    doctorPassword = request.form.get('doctorPassword')
-
-    doctor = Doctor.query.filter_by(doctorUsername=doctorUsername).first()
-    if doctor:
-        if bcrypt.check_password_hash(doctor.doctorPassword,doctorPassword):
-            # login_user(doctor)
-            return redirect(url_for('dashboard'))
-        else:
-            flash('Please check your login details and try again')
-            return redirect(url_for('doctorLogIn'))
-    else:
-        flash('User does not exist')
-        return redirect(url_for('doctorSignIn'))      
 
 @app.route('/logout', methods=['GET','POST'])
 @login_required
--- Flask_React/flask-server/api/models.py
+++ Flask_React/flask-server/api/models.py
@@ -1,15 +1,40 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import UserMixin 
 
 db = SQLAlchemy()
+#user data model 
+#Role data model 
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    
+class User(db.Model,UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(1000))
+    email = db.Column(db.String(255), unique=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False, server_default = '')
+    active = db.Column(db.Boolean())
+    roles = db.relationship('Role', secondary='user_roles' , backref='user')
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
 
+# Define the user_roles association table
+user_roles = db.Table('user_roles',
+    db.Column('user_id', db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE')),
+)
+    
 class Patient(db.Model , UserMixin):
     id = db.Column(db.Integer , primary_key=True)
     name = db.Column(db.String(1000))
     email = db.Column(db.String(100))
     username = db.Column(db.String(20), unique=True)
     password = db.Column(db.String(100))
+   
 
 class Doctor(db.Model, UserMixin):
     doctorId = db.Column(db.Integer , primary_key=True)
